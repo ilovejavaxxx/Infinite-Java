f994d76dc7fb67723cee4afe6da2a8a7630960c0
ilovejavaxxx <86233513+ilovejavaxxx@users.noreply.github.com>
2021-06-23 16:47:57 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+// I have did this before but why not try again cause it's funnnnnnnnn
+
+public class {
+  public static void main(String[] args) {
+    for (;;) System.out.print("Java!!!");
+  }
+}
